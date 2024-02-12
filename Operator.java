1bc8ba321daabd3e90222df8af7026b840a210e1
ballin <a0955771728@gmail.com>
2024-02-12 12:18:43 +0900

    基本運算符號

---
+++ Operator.java
@@ -0,0 +1,45 @@
+public class Operator{
+    public static void main(String[] args){
+        // // 算術運算
+        // int x=5+(3/4);
+        // System.out.println(x);
+        // double y=5/2.0; //錯誤：2.0為 double 型態，無法放進 int，必須宣告成 double，輸出為2.5
+        // System.out.println(y);
+        // double z=5%3.5;
+        // System.out.println(z); // 取餘數，1.5
+
+        // // 指定運算
+        // int x=5;
+        // x=x+2; // 7
+        // System.out.println(x);
+        // int y=3;
+        // y+=2;
+        // y*=3;
+        // System.out.println(y); // 15
+
+        // 比較運算（結果會是布林值）
+        // boolean x = 5<=5;
+        // System.out.println(x); // true
+        // boolean y = 2!=2;
+        // System.out.println(y); // false
+        // boolean z = "Hello"=="hello";
+        // System.out.println(z); // false
+
+        // // 單元運算
+        // int x=4;
+        // x++; // x=x+1; // x+=1;
+        // x--; // x=x-1; // x-=1;
+        // System.out.println(x); // 4
+        // boolean y=!true;
+        // System.out.println(y); // false
+
+        // // 邏輯運算
+        // boolean x=true&&false; // 且，兩者都得滿足，其中1個 false ，那就是 false
+        // System.out.println(x); // false
+        // boolean y=false||true; // 或，其中一邊滿足即可，其中1個 true，那就是 true
+        // System.out.println(y); // true
+        // boolean z=false||false;
+        // System.out.println(z); // false
+
+    }
+}
