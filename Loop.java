56361fb389cc455e717be7afe477e6785af6f837
ballin <a0955771728@gmail.com>
2024-02-15 20:56:15 +0900

    流程控制：迴圈基礎

---
+++ Loop.java
@@ -0,0 +1,37 @@
+public class Loop {
+    public static void main(String[] args) {
+        // while 迴圈
+        // 無窮迴圈
+        // while(true){
+        //     System.out.println("Loop");
+        // }
+
+        // 可以被控制運作次數的迴圈
+        // int x = 2;
+        // while (x <= 6) {
+        //     System.out.println(x);
+        //     x = x + 2;
+        // }
+
+        // 計算1+2+3+...+100的總和
+        // int n = 1;
+        // int total = 0;
+        // while (n <= 100) {
+        //     total = total + n;
+        //     n++;
+        // }
+        // System.out.println(total);
+
+        // for 迴圈
+        // for(int x=2; x<=6;x=x+2){
+        //     System.out.println(x);
+        // }
+
+        // 計算1+2+3+...+100的總和
+        // int total = 0;
+        // for(int n=1;n<=100;n++){
+        //     total += n;
+        // }
+        // System.out.println(total);
+    }
+}
